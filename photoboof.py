7a055f1b3cc8c58fc9aa10620537ec65f79cb305
dashdrum <dan@dashdrum.com>
2015-06-28 10:22:15 -0400

    Code cleanup and comments - Added TODO steps

--- photoboof.py
+++ photoboof.py
@@ -76,7 +76,7 @@ def is_connected():
 
 def upload_montage(file_prefix):
 
-    led_on(upload_led)  ## LED 4 - uploading
+    led_on(processing_led)  ## LED 4 - uploading
 
     connected = is_connected()
     if connected:
@@ -112,9 +112,11 @@ def upload_montage(file_prefix):
     # else:
         # print "No connection - will upload later"
 
-    led_off(upload_led)  
+    led_off(processing_led)  
 
 def photo_process():
+
+    ## Camera setup
     camera = picamera.PiCamera()
     camera.resolution = (640,480) 
     #camera.resolution = (2592,1944)
@@ -123,8 +125,9 @@ def photo_process():
     camera.saturation = 0
     camera.start_preview()
 
-
+    ## Pose coundown
     print "Pose" 
+    # TODO: Superimpose message: get your pose ready - will take 4 quick photos
 
     led_off(ready_led)
     for runcount in range(0,5):
@@ -134,24 +137,30 @@ def photo_process():
         led_off(pose_led)
         sleep(0.300)
 
-    print "Take Pics"
+    ## Taking photos
+    print "Take Photos"
     file_prefix = time.strftime("%Y%m%d%H%M%S")
     led_on(take_led)  
+    # TODO: Superimpose '1'
     with camera:
         try:
             for i, filename in enumerate(camera.capture_continuous(FILE_PATH + file_prefix + '{counter:02d}.jpg', 
                                          format=None, use_video_port=False, resize=None, splitter_port=0)):
                 led_off(take_led)
+                # TODO: Turn off superimposed number
                 # print(filename)
                 time.sleep(1)
                 if i == PHOTO_COUNT - 1:
                     break
                 led_on(take_led)
+                # TODO: Superimpose i
         finally:
             camera.stop_preview()
             camera.close()
 
-    led_on(upload_led) ## LED 3 - Making montage
+    # TODO: Display last photo taken  -  FILE_PATH + file_prefix + '04.jpg'
+
+    led_on(processing_led) ## LED 3 - Making montage
     print "Making montage"
 
     ## Make animated GIF
@@ -159,24 +168,33 @@ def photo_process():
     os.system(gm) #make the .gif
 
     ## Make montage with text on the side
+    # make the grid
     gm = "gm montage -tile 2x -geometry 640x480+5+5 " + FILE_PATH + file_prefix + "*.jpg " + MONTAGE_PATH + file_prefix + "_grid.jpg"
-    os.system(gm) #make the montage
+    os.system(gm) 
+    # create text box
     gm ='gm convert -size 980x170 xc:#ffffff -pointsize 60 -font Arial -fill black -draw "text 30,105 \'' + EVENT + '\'" -pointsize 16 -draw "text 850,25 \'' + file_prefix + '\'" text.jpg'
-    os.system(gm) #create text box
+    os.system(gm) 
+    # spin it
     gm = 'gm convert -rotate "270>" text.jpg text.jpg'
-    os.system(gm) # spin it
+    os.system(gm) 
+    # join text and grid
     gm = 'gm montage -geometry x980+0  text.jpg -gravity west ' + MONTAGE_PATH + file_prefix + '_grid.jpg -gravity east  -resize x980 '+ MONTAGE_PATH + file_prefix + '_grid.jpg'
-    os.system(gm) # join text and grid
+    os.system(gm) 
+    # chop off the extra
     gm = 'gm convert ' + MONTAGE_PATH + file_prefix + '_grid.jpg -crop 1470x980+1131+0 ' + MONTAGE_PATH + file_prefix + '_grid.jpg'
-    os.system(gm)  # chop off the extra
+    os.system(gm)  
 
-    led_off(upload_led)
+    led_off(processing_led)
 
     ## Upload Montage
 
     upload_montage(file_prefix)
 
-    ## Blink LEDs to show complete
+    ## Finished
+
+    # TODO: Display GIF  -  FILE_PATH + file_prefix + ".gif"
+
+    # Blink LEDs to show complete
 
     for runcount in range(0,3):
 
@@ -188,6 +206,7 @@ def photo_process():
         sleep(0.300)
 
     print "Done"
+    # TODO: Superimpose message: Press Button to Begin
 
     led_on(ready_led)
 
@@ -206,31 +225,38 @@ def batch_upload():
     # print 'jpg_list:', jpg_list
 
     if jpg_count > 0:
+        led_on(processing_led)
         # print 'Uploading pending photos '
         print 'Please wait'
 
         for j in jpg_list:
             upload_montage(j)
 
+        led_off(processing_led)
 
+#-----------------------------------------------------------------------------#
+# Constants
 
 ALBUM = 'Test Album'
-EVENT = 'Dawn-a-pa-Drew-za 2015'
+EVENT = 'Event Name Goes Here'
 FILE_PATH = 'pics/'
 MONTAGE_PATH = FILE_PATH + 'montages/'
 UPLOADED_PATH = FILE_PATH + 'uploaded/'
 GIF_DELAY = 50 
 PHOTO_COUNT = 4
 
-
-
-
 led_count = 4
 ready_led = 0
 pose_led = 1
 take_led = 2
-upload_led = 3
+processing_led = 3
 btn_pin = wP2board(5)
+
+# Power up
+
+## TODO: Display title card
+
+# GPIO Setup
 
 GPIO.setwarnings(False)
 
@@ -242,12 +268,19 @@ GPIO.setup(btn_pin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 led_powerup_test()
 
+# Check for pending uploads
+
 connected = is_connected()
 if connected:
     batch_upload()
 
+# Let's do this!!!!
+
 print "Ready"
 led_on(ready_led)
+# TODO: Superimpose 'Press Button to Begin'
+
+# Button loop
 
 while True:
     GPIO.wait_for_edge(btn_pin, GPIO.FALLING)
